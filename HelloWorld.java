3ad5ba75fab770ca877bdb0848576a2ece63fa48
astikamohinta <astika.mohinta@wipro.com>
2020-04-29 16:45:53 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {//class
+	public static void main(String args[]) {//method
+		HelloWorld hw= new HelloWorld();//object.can be written n number of times
+		//HelloWorld hw =new HelloWorld();//object of the class.New is the keyword for object
+		System.out.println(args[0]);
+		//System.out.println(hw);
+		
+	
+	
+	
+		
+	}
+}
+
+                 
